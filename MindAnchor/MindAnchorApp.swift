8bda47f13dfe13580373befec7b9f6c8e1eb3433
Meik Eisenbraun <meik.eisenbraun@iu-study.org>
2026-08-14 10:14:27 +0200

    initial Xcode project setup with SwiftData and basic App entry

---
+++ MindAnchor/MindAnchorApp.swift
@@ -0,0 +1,18 @@
+//
+//  MindAnchorApp.swift
+//  MindAnchor
+//
+//  Created by Meik Eisenbraun on 14.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct MindAnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("MindAnchor - Merklisten-App")
+                .font(.headline)
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# MindAnchor
+
+Native iOS-Merklistenanwendung mit SwiftData und SwiftUI.
+Modul: Apple Mobile Solution Development II (DLAMSD02)
+Studierender: Meik Eisenbraun (IU14121268)
